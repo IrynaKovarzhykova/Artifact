0439043cf74df9c4950e039262c51090565c0b2f
IrynaKovarzhykova <kovarzhikova@gmail.com>
2024-07-18 11:42:40 +0300

    Add files via upload

---
+++ Artifact/src/main/java/org/example/SimpleCalculator.java
@@ -0,0 +1,27 @@
+package org.example;
+
+public class SimpleCalculator {
+    public double add(double a, double b, double c) {
+        return a + b + c;
+    }
+
+    public double subtract(double a, double b, double c) {
+        return a - b - c;
+    }
+
+    public double multiply(double a, double b, double c) {
+        return a * b * c;
+    }
+
+
+    public double divide(double a, double b, double c) {
+
+
+        if (b == 0 || c == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return a / b / c;
+    }
+}
+
+
